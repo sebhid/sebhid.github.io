e35d7db98591375f4c0053c8f820a5b7e4e4e47f
Sebastian Hidvégi <sebastian.hidvegi@pori.fi>
2022-02-17 12:39:26 +0200

    navigation JS
    added JS-script to handle navigation bar

---
+++ webcampori/scripts/nav.js
@@ -0,0 +1,14 @@
+var html = '<li><a href=ndex.html>Kirjurinluoto</a></li>\
+			<li><a href=maauimala.html>Maauimala</a></li>\
+			<li><a href=maauimala2.html>Isomäen latukamera</a></li>\
+			<li><a href=kauppatori.html>Porin kauppatori</a></li>\
+			<li><a href=puuvilla.html>Puuvilla</a></li>\
+			<li><a href=reposaari.html>Reposaari</a></li>\
+			<li><a href=yyteri.html>Yyteri</a></li>\
+			<li><a href=munakari.html>Munakari</a></li>\
+			<li><a href=noormarkku.html>Noormarkun kirkkokallio</a></li>\
+			<li><a href=vaharauma.html>Vähä-Rauman koulun työmaa</a></li>\
+			<li><a href=etelaranta.html>Eteläranta</a></li>'
+;
+
+document.getElementById('nav').innerHTML = html;
